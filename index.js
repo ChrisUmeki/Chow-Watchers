f1e0c4d3be7f9e675fea3241f2524274b55e7068
Chris Umeki <ctu3@cornell.edu>
2018-01-27 15:10:55 -0500

    d3 scatter plot

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 let express = require('express');
 let app = express();
 app.use(express.static('styles'));
-app.use(express.static('fakedata.json'));
+app.use(express.static('/fake.csv'));
 let path = require('path');
+let d3 = require('d3');
 
 let bodyParser = require('body-parser');
 let urlencodedParser = bodyParser.urlencoded({ extended: true });
-
+app.use(bodyParser.json());//do I need?
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/result', urlencodedParser, (req, res) => {
+
+
+app.get('/result', urlencodedParser, (req, res) => {
   //let name = req.body.pet-name-input;
   console.log(req.body);
 
-  res.send();
+  res.sendFile(path.join(__dirname + '/result.html'));
+
+
 });
 
 app.listen(8081, () => console.log('Listening on port 8081!'));

205ed174393f90a58007bb9b8434f680c47f62b9
Amy Zhong <az287@cornell.edu>
2018-01-27 20:41:22 -0500

    Placed graph above text, added text to results page

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ app.use(express.static('styles'));
 app.use(express.static('/fake.csv'));
 app.use(express.static('assets'));
 let path = require('path');
-let d3 = require('d3');
 let csv = require('csv');
 
 let bodyParser = require('body-parser');
@@ -21,7 +20,7 @@ app.get('/result', urlencodedParser, (req, res) => {
   //let name = req.body.pet-name-input;
   console.log(req.query);
 
-   
+
   res.sendFile(path.join(__dirname + '/result.html'));
 
 
